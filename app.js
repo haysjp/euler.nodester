c6ac12374cfe0e1a3d72bc58f59a6f9d114ae35b
Matt Hernandez <matt@eventargs.com>
2012-07-30 19:56:48 -0400

    Initial commit with the entire solution complete

---
+++ app.js
@@ -0,0 +1,82 @@
+
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , fs = require('fs');
+
+var app = express();
+
+/**
+ * Server configuration.
+ */
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// Handle the request to base.
+app.get('/', function(req, res) {
+  
+  // Magic!
+  var start = new Date().getTime();
+  var score = calculateScore('names.txt');
+  var end = new Date().getTime();
+  
+  res.render('index', {
+      title: 'Project Euler - Problem 22'
+    , time: end - start
+    , score: score
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+/**
+ * Calculate the score for the names in the specified file.
+ * @param {String} fileName The file with the names.
+ * @returns The total score of all names in the file.
+ * @type Number
+ */
+function calculateScore (fileName) {
+  var score = 0
+    , names = []
+    , cleanIndex = 0
+    , alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+  try {
+    var nameData = fs.readFileSync(fileName, 'ascii').split(',');
+  } catch (err) {
+    console.error('Unable to read file :(');
+    console.log(err);
+  }
+  
+  // Sort then calculate the score.
+  nameData.sort().forEach(function(name, index) {
+    thisScore = 0;
+    for (var i=0; i < name.length; i++) {
+      // The score for the current name.
+      thisScore += alpha.indexOf(name[i].toLowerCase()) + 1;
+    };
+    
+    // Add the index multiplier.
+    score += thisScore * (index + 1);
+  });
+  
+  return score;
+}
